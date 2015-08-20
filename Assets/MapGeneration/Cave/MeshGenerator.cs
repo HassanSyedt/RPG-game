1355d54252fcda0e2740cc7d0d48e1c37f2f1722
HassanSyedt <hmobeen@umass.edu>
2015-08-19 23:49:27 -0400

    Generators
    Start of the generation scripts for pg generated levels

---
+++ Assets/MapGeneration/Cave/MeshGenerator.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using JetBrains.Annotations;
+
+public class MeshGenerator : MonoBehaviour
+{
+
+    public SquareGrid squareGrid;
+
+    public void GenerateMesh(int[,] map, float squareSize)
+    {
+        squareGrid = new SquareGrid(map,squareSize);
+    }
+   
+
+	public class  Square
+	{
+	    public ControlNode TopLeftNode, TopRightNode, BottomLeftNode, BottomRightNode;
+	    public Node CenterTopNode, CenterRightNode, CenterBottomNode, CenterLeftNode;
+
+	    public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
+	    {
+	        TopLeftNode = _topLeft;
+	        TopRightNode = _topRight;
+	        BottomRightNode = _bottomRight;
+	        BottomLeftNode = _bottomLeft;
+
+	        CenterTopNode = TopLeftNode.Right;
+	        CenterRightNode = BottomRightNode.Above;
+	        CenterBottomNode = BottomLeftNode.Right;
+	        CenterLeftNode = BottomLeftNode.Above;
+	    }
+	   
+	}
+     public class SquareGrid
+     {
+         public Square[,] squares;
+
+         public SquareGrid(int[,] map, float squareSize)
+         {
+             int nodeCountX = map.GetLength(0);
+             int nodeCountY = map.GetLength(1);
+             float mapWidth = nodeCountX*squareSize;
+             float mapHeight = nodeCountY*squareSize;
+
+             ControlNode [,] controlNode = new ControlNode[nodeCountX,nodeCountY];
+
+             for (int x = 0; x < nodeCountX; x++)
+             {
+                 for (int y = 0; y < nodeCountY; y++)
+                 {
+                     Vector3 pos= new Vector3(-mapWidth/2 + x*squareSize +squareSize/2,0,-mapHeight/2 +y*squareSize +squareSize/2);
+                     controlNode[x,y]=new ControlNode(pos,map[x,y]==1,squareSize);
+                 }
+             }
+             squares= new Square[nodeCountX-1,nodeCountY-1];
+             for (int x = 0; x < nodeCountX-1; x++)
+             {
+                 for (int y = 0; y < nodeCountY-1; y++)
+                 {
+                     squares[x,y]= new Square(controlNode[x,y+1],controlNode[x+1,y+1],controlNode[x+1,y],controlNode[x,y]);
+                 }
+             }
+         }
+     }
+
+    public class Node
+    {
+        public Vector3 Position;
+        public int VertexIndex = -1;
+
+        public Node(Vector3 _pos)
+        {
+            Position = _pos;
+        }
+    }
+
+    public class ControlNode : Node
+    {
+        public bool Active;
+        public Node Above, Right;
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos)
+        {
+            Active = _active;
+            Above= new Node(Position+Vector3.forward*squareSize/2f);
+            Right = new Node(Position + Vector3.right*squareSize/2f);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (squareGrid != null)
+        {
+             for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+             {
+                 for (int y = 0; y < squareGrid.squares.GetLength(1) ; y++)
+                 {
+                     Gizmos.color = (squareGrid.squares[x, y].TopLeftNode.Active) ? Color.black : Color.white;
+                     Gizmos.DrawCube(squareGrid.squares[x, y].TopLeftNode.Position, Vector3.one * .4f);
+
+                     Gizmos.color = (squareGrid.squares[x, y].TopRightNode.Active) ? Color.black : Color.white;
+                     Gizmos.DrawCube(squareGrid.squares[x, y].TopRightNode.Position, Vector3.one * .4f);
+
+                     Gizmos.color = (squareGrid.squares[x, y].BottomRightNode.Active) ? Color.black : Color.white;
+                     Gizmos.DrawCube(squareGrid.squares[x, y].BottomRightNode.Position, Vector3.one * .4f);
+
+                     Gizmos.color = (squareGrid.squares[x, y].BottomLeftNode.Active) ? Color.black : Color.white;
+                     Gizmos.DrawCube(squareGrid.squares[x, y].BottomLeftNode.Position, Vector3.one * .4f);
+
+
+                     Gizmos.color = Color.grey;
+                     Gizmos.DrawCube(squareGrid.squares[x, y].CenterTopNode.Position, Vector3.one * .15f);
+                     Gizmos.DrawCube(squareGrid.squares[x, y].CenterRightNode.Position, Vector3.one * .15f);
+                     Gizmos.DrawCube(squareGrid.squares[x, y].CenterBottomNode.Position, Vector3.one * .15f);
+                     Gizmos.DrawCube(squareGrid.squares[x, y].CenterLeftNode.Position, Vector3.one * .15f);
+                 }
+             }
+        }
+    }
+}
---
+++ Assets/MapGeneration/Dungeon/TestGeneration.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestGeneration : MonoBehaviour
+{
+
+    private const int NODE = 1;
+    private const int EMPTY = 0;
+
+    public int randomFillPercent;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    int[,] grid;
+    private Dictionary<Node, LinkedList<Node>> graph;
+
+    public int width, height;
+    // Use this for initialization
+    void Start()
+    {
+        grid = new int[width, height];
+        for (int x = 0; x < width; x++)
+        {
+
+            for (int y = 0; y < height; y++)
+            {
+                grid[x, y] = EMPTY;
+            }
+        }
+        int xR = Random.Range(0, width);
+        int yR = Random.Range(0, height);
+        grid[xR, yR] = NODE;
+        Node root = new Node(xR, yR);
+        graph = new Dictionary<Node, LinkedList<Node>>();
+        graph.Add(root, createRandomNodes());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            createRandomNodes();
+
+    }
+
+    public struct Node
+    {
+        int xCoordinate;
+        int yCoordinate;
+
+        public Node(int x, int y)
+        {
+            xCoordinate = x;
+            yCoordinate = y;
+        }
+    }
+
+    public LinkedList<Node> createRandomNodes()
+    {
+        LinkedList<Node> edges = new LinkedList<Node>();
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    grid[x, y] = EMPTY;
+                }
+                else
+                {
+                    if (pseudoRandom.Next(0, 100) < randomFillPercent)
+                    {
+                        grid[x, y] = NODE;
+                        Node n = new Node(x, y);
+                        edges.AddLast(n);
+
+                    }
+                    else
+                    {
+                        grid[x, y] = EMPTY;
+                    }
+                }
+            }
+        }
+        return edges;
+    }
+
+
+
+    void OnDrawGizmos()
+    {
+        if (grid != null)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Gizmos.color = (grid[x, y] == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+
+                }
+            }
+        }
+    }
+
+		
+	}
+	
+	
+
+
